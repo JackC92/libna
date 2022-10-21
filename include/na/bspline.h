97c5d730e093365b7a4ca4359f922d3051c624e5
Shukui Chen <shukui.chen@mail.utoronto.ca>
2022-10-20 21:37:43 -0400

    bspline: overloaded evaluate to use user-provided user functions

--- include/na/bspline.h
+++ include/na/bspline.h
@@ -32,6 +32,56 @@ namespace na
 			const int deg,
 			const int m,
 			const Eigen::VectorXd& s);
+
+		template <typename Node, typename MemPtr>
+		Eigen::Vector3d evaluate(
+			const std::vector<Node>& nodes,
+			const Eigen::VectorXd& T,
+			const int deg,
+			const int m,
+			const double s,
+			MemPtr func)
+		{
+			static_assert(std::is_member_function_pointer_v<MemPtr>, "func must be a non-static member function");
+
+			auto fn = std::mem_fn(func);
+
+			Eigen::VectorXd b;
+			int mu;
+			evaluate_basis(T, deg, m, s, mu, b);
+
+			Eigen::Vector3d val(0.0, 0.0, 0.0);
+			for (int i = 0; i < deg + 1; ++i)
+			{
+				val += fn(nodes[mu - deg + i]) * b(i);
+			}
+			return val;
+		}
+
+		template <typename Node, typename MemPtr>
+		Eigen::Vector3d evaluate(
+			const std::vector<std::shared_ptr<Node>>& nodes,
+			const Eigen::VectorXd& T,
+			const int deg,
+			const int m,
+			const double s,
+			MemPtr func)
+		{
+			static_assert(std::is_member_function_pointer_v<MemPtr>, "func must be a non-static member function");
+
+			auto fn = std::mem_fn(func);
+
+			Eigen::VectorXd b;
+			int mu;
+			evaluate_basis(T, deg, m, s, mu, b);
+
+			Eigen::Vector3d val(0.0, 0.0, 0.0);
+			for (int i = 0; i < deg + 1; ++i)
+			{
+				val += fn(nodes[mu - deg + i].get()) * b(i);
+			}
+			return val;
+		}
 	}
 }
 

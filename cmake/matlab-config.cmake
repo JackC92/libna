31bdb7d37aa328eb3b41387ff415e66e1861008c
Shukui Chen <chenshukui@hotmail.com>
2023-04-02 22:32:54 -0400

    Update project files

--- cmake/matlab-config.cmake
+++ cmake/matlab-config.cmake
@@ -1,17 +1,17 @@
 # This minimal CMake Config Mode file for imporing MATLAB can be used anywhere if the environment variable MATLABROOT is set.
 add_library(Matlab::Engine UNKNOWN IMPORTED)
 set_target_properties(Matlab::Engine PROPERTIES
-	INTERFACE_INCLUDE_DIRECTORIES "$ENV{MATLABROOT}/extern/include"
-	IMPORTED_LOCATION "$ENV{MATLABROOT}/extern/lib/win64/microsoft/libMatlabEngine.lib")
-	
+    INTERFACE_INCLUDE_DIRECTORIES "$ENV{MATLABROOT}/extern/include"
+    IMPORTED_LOCATION "$ENV{MATLABROOT}/extern/lib/win64/microsoft/libMatlabEngine.lib")
+    
 add_library(Matlab::DataArray UNKNOWN IMPORTED)
 set_target_properties(Matlab::DataArray PROPERTIES
-	INTERFACE_INCLUDE_DIRECTORIES "$ENV{MATLABROOT}/extern/include"
-	IMPORTED_LOCATION "$ENV{MATLABROOT}/extern/lib/win64/microsoft/libMatlabDataArray.lib")
+    INTERFACE_INCLUDE_DIRECTORIES "$ENV{MATLABROOT}/extern/include"
+    IMPORTED_LOCATION "$ENV{MATLABROOT}/extern/lib/win64/microsoft/libMatlabDataArray.lib")
 
 add_library(Matlab::matlab INTERFACE IMPORTED)
 set_property(TARGET Matlab::matlab PROPERTY
-	INTERFACE_LINK_LIBRARIES Matlab::Engine Matlab::DataArray)
+    INTERFACE_LINK_LIBRARIES Matlab::Engine Matlab::DataArray)
 
 set(Matlab_INCLUDES $ENV{MATLABROOT}/extern/include)
 set(Matlab_LIBRARIES Matlab::matlab)
--- cmake/tensorflow-config.cmake
+++ cmake/tensorflow-config.cmake
@@ -3,8 +3,8 @@ get_filename_component(_TensorFlow_PREFIX "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
 add_library(TensorFlow::tensorflow STATIC IMPORTED)
 set_target_properties(TensorFlow::tensorflow PROPERTIES
-	INTERFACE_INCLUDE_DIRECTORIES "${_TensorFlow_PREFIX}/include"
-	IMPORTED_LOCATION "${_TensorFlow_PREFIX}/lib/tensorflow.lib")
+    INTERFACE_INCLUDE_DIRECTORIES "${_TensorFlow_PREFIX}/include"
+    IMPORTED_LOCATION "${_TensorFlow_PREFIX}/lib/tensorflow.lib")
 
 set(TensorFlow_INCLUDES ${_TensorFlow_PREFIX}/include)
 set(TensorFlow_LIBRARIES TensorFlow::tensorflow)
